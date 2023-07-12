b3a5c596cd1ca5fc985d21432f431be02fa7949b
r0astPiGGy <1forzyg1@gmail.com>
2023-07-12 16:28:39 +0300

    Add ViewPort, ViewPortLayout, fix FreeScroll

--- src/jvmMain/kotlin/com/rodev/jbpkmp/WelcomeScreen.kt
+++ src/jvmMain/kotlin/com/rodev/jbpkmp/WelcomeScreen.kt
@@ -1,30 +1,20 @@
 package com.rodev.jbpkmp
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.chihsuanwu.freescroll.freeScroll
-import com.chihsuanwu.freescroll.rememberFreeScrollState
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import com.rodev.jbpkmp.presentation.ViewPort
 import kotlin.math.roundToInt
 
 @Composable
@@ -33,34 +23,11 @@ fun WelcomeScreen() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     Surface {
-        NodeTest()
-    }
-}
-
-@Composable
-fun NodeTest() {
-    val scrollState = rememberFreeScrollState()
-    Box(
-        modifier = Modifier
-            .clipToBounds()
-            .fillMaxSize()
-            .border(
-                border = BorderStroke(4.dp, Color.Red),
-            )
-            .freeScroll(scrollState)
-    ) {
-        Box(
+        ViewPort(
             modifier = Modifier
-                .wrapContentSize(align = Alignment.TopStart, unbounded = true)
-                .requiredSize(
-                    width = 32766.dp,
-                    height = 32766.dp
-                )
-                .border(
-                    border = BorderStroke(16.dp, Color.Yellow),
-                )
+                .fillMaxSize()
         ) {
-                DraggableTextLowLevel()
+            DraggableTextLowLevel()
         }
     }
 }
@@ -72,7 +39,7 @@ private fun DraggableTextLowLevel() {
 
     Box(
         Modifier
-            .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+            .absoluteOffset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
             .background(Color.Blue)
             .size(50.dp)
             .pointerInput(Unit) {
---
+++ src/jvmMain/kotlin/com/rodev/jbpkmp/presentation/ViewPort.kt
@@ -0,0 +1,47 @@
+package com.rodev.jbpkmp.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.layout.Layout
+import com.chihsuanwu.freescroll.freeScroll
+import com.chihsuanwu.freescroll.rememberFreeScrollState
+
+@Composable
+fun ViewPort(
+    modifier: Modifier = Modifier,
+    initialX: Int = 0,
+    initialY: Int = 0,
+    content: @Composable () -> Unit
+) {
+    val scrollState = rememberFreeScrollState(
+        initialX = initialX,
+        initialY = initialY
+    )
+    Box(
+        modifier = modifier
+            .clipToBounds()
+            .freeScroll(scrollState)
+    ) {
+        ViewPortLayout(content = content)
+    }
+}
+
+@Composable
+fun ViewPortLayout(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Layout(
+        content = content,
+        modifier = modifier,
+    ) { measurables, constraints ->
+        val placeables = measurables.map {
+            it.measure(constraints)
+        }
+
+        layout(Int.MAX_VALUE, Int.MAX_VALUE) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = 0)
+            }
+        }
+    }
+}
