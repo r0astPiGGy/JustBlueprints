aa95cc9afc9a9bf9b0a80b72b7ca5d1213497318
r0astPiGGy <1forzyg1@gmail.com>
2023-07-13 13:04:13 +0300

    Add Pins, fix Node layout

--- src/jvmMain/kotlin/com/rodev/jbpkmp/Main.kt
+++ src/jvmMain/kotlin/com/rodev/jbpkmp/Main.kt
@@ -3,7 +3,7 @@ package com.rodev.jbpkmp
 import androidx.compose.material.Scaffold
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.rodev.jbpkmp.presentation.screens.WelcomeScreen
+import com.rodev.jbpkmp.presentation.screens.welcome_screen.WelcomeScreen
 import com.rodev.jbpkmp.theme.AppTheme
 
 fun main() = application {
--- src/jvmMain/kotlin/com/rodev/jbpkmp/presentation/components/Node.kt
+++ src/jvmMain/kotlin/com/rodev/jbpkmp/presentation/components/Node.kt
@@ -1,42 +1,41 @@
 package com.rodev.jbpkmp.presentation.components
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.rodev.jbpkmp.theme.AppTheme
+import kotlin.math.max
+import kotlin.math.roundToInt
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         AppTheme(useDarkTheme = true) {
             Surface {
-//                ViewPort(
-//                    modifier = Modifier
-//                        .fillMaxSize(),
-//                    viewPortModifier = Modifier
+                ViewPort(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    viewPortModifier = Modifier
 //                        .drawWithCache {
 //                            onDrawWithContent {
 //
@@ -53,11 +52,6 @@ fun main() = application {
 //                                )
 //                            }
 //                        }
-//                ) {
-//                    Node()
-//                }
-                Column(
-                    modifier = Modifier.fillMaxSize()
                 ) {
                     Node()
                 }
@@ -66,73 +60,20 @@ fun main() = application {
     }
 }
 
+private const val nodeOutlinePadding = 6
+private const val pinSize = 15
+
 @Composable
 @Preview
 fun Node() {
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
 
-    val outlinePadding = 8.dp
-
-//    Card(
-//        shape = RoundedCornerShape(8.dp),
-//        modifier = Modifier
-////            .size(width = 250.dp, 140.dp)
-//            .absoluteOffset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
-//            .wrapContentSize()
-//            .pointerInput(Unit) {
-//                detectDragGestures { change, dragAmount ->
-//                    change.consume()
-//                    offsetX += dragAmount.x
-//                    offsetY += dragAmount.y
-//                }
-//            }
-//    ) {
-//        Column(
-//            modifier = Modifier
-//                .wrapContentSize()
-//        ) {
-//            // Node Header
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(40.dp)
-//                    .background(Color.Blue)
-//                    .padding(outlinePadding)
-//            ) {
-//                Text(
-//                    text = "Header",
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                )
-//            }
-//            Row(
-//                modifier = Modifier
-//                    .padding(outlinePadding),
-//                horizontalArrangement = Arrangement.SpaceBetween
-//            ) {
-//                // Input Pins
-//                Column(
-//                    modifier = Modifier
-//                        .requiredSize(300.dp)
-//                        .background(Color.Yellow)
-//                ) {
-//
-//                }
-//                // Output Pins
-//                Column(
-//                    modifier = Modifier
-//                        .requiredSize(400.dp)
-//                        .background(Color.Red)
-//                ) {
-//
-//                }
-//            }
-//        }
-//    }
-    Column(
+    Card(
+        shape = RoundedCornerShape(8.dp),
         modifier = Modifier
-            .background(MaterialTheme.colors.background)
+            .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+            .wrapContentSize()
             .pointerInput(Unit) {
                 detectDragGestures { change, dragAmount ->
                     change.consume()
@@ -141,40 +82,131 @@ fun Node() {
                 }
             }
     ) {
-        Row(
+        NodeBody(
             modifier = Modifier
-                .background(Color.Blue)
+                .background(MaterialTheme.colors.background)
+                .defaultMinSize(minWidth = 100.dp)
+                .wrapContentHeight()
         ) {
-            Icon(Icons.Outlined.Home, contentDescription = null)
-
-            Text("Headeафутафуафуаирфуаирфуаирфуаифруаифруаирфуr")
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Box(
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top,
                 modifier = Modifier
-                    .size(100.dp)
-                    .background(Color.Yellow)
-            )
+                    .background(Color.Blue)
+                    .padding(nodeOutlinePadding.dp)
+                    .padding(end = 50.dp)
+            ) {
+                Text(
+                    text = "Header",
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+            }
 
-            Box(
+            PinContainer(
                 modifier = Modifier
-                    .size(150.dp)
-                    .background(Color.Yellow)
-            )
+                    .wrapContentSize(align = Alignment.TopStart)
+            ) {
+                InputPin()
+            }
+
+            PinContainer (
+                modifier = Modifier
+                    .wrapContentSize(align = Alignment.TopEnd)
+            ) {
+                OutputPin()
+            }
         }
     }
 }
 
 @Composable
-fun InputPin() {
+private fun NodeBody(modifier: Modifier, content: @Composable () -> Unit) {
+    Layout(
+        content = content,
+        modifier = modifier,
+        measurePolicy = nodeBodyMeasurePolicy()
+    )
+}
 
+@Composable
+private fun nodeBodyMeasurePolicy(): MeasurePolicy = remember {
+    MeasurePolicy { measurables, constraints ->
+        val inputContainer = measurables[1].measure(constraints)
+        val outputContainer = measurables[2].measure(constraints)
+
+        val containersWidth = inputContainer.width + outputContainer.width
+
+        val header = measurables[0].measure(constraints.copy(minWidth = containersWidth))
+
+        val spaceBetweenContainers = max(0, header.width - containersWidth)
+        val height = header.height + max(inputContainer.height, outputContainer.height)
+        val width = max(header.width, containersWidth)
+
+        layout(width = width, height = height) {
+            header.place(0, 0)
+            inputContainer.place(0, header.height)
+            outputContainer.place(inputContainer.width + spaceBetweenContainers, header.height)
+        }
+    }
+}
+
+@Composable
+private fun PinContainer(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column (
+        content = content,
+        modifier = modifier
+            .defaultMinSize(minWidth = 50.dp, minHeight = 50.dp)
+            .padding(nodeOutlinePadding.dp)
+    )
+}
+
+@Composable
+fun InputPin() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Pin(color = Color.Red)
+        Spacer(modifier = Modifier.size(6.dp))
+        Column(
+            modifier = Modifier.requiredSizeIn(maxWidth = 180.dp)
+        ) {
+            Text(
+                text = "Input pin"
+            )
+//            var inputText by remember { mutableStateOf("") }
+//            TextField(value = inputText, onValueChange = { inputText = it }, singleLine = true)
+        }
+    }
 }
 
 @Composable
 fun OutputPin() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.requiredSizeIn(maxWidth = 180.dp)
+        ) {
+            Text(
+                text = "Return value"
+            )
+        }
+        Spacer(modifier = Modifier.size(6.dp))
+        Pin(color = Color.Red, connected = true)
+    }
+}
 
+@Composable
+fun Pin(
+    color: Color,
+    connected: Boolean = false
+) {
+    Canvas(modifier = Modifier.size(pinSize.dp)) {
+
+        drawCircle(color = color, style = if (connected) Fill else Stroke(width = 2f))
+    }
 }
