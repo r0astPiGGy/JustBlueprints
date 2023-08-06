6cea61d9f28a04f9a636318fa9892e099bbf98f3
Dewerro <funcake0@gmail.com>
2023-08-06 18:03:06 +0300

    Add colors for nodes

--- src/jvmMain/kotlin/com/rodev/jbpkmp/theme/Color.kt
+++ src/jvmMain/kotlin/com/rodev/jbpkmp/theme/Color.kt
@@ -2,7 +2,16 @@ package com.rodev.jbpkmp.theme
 
 import androidx.compose.ui.graphics.Color
 
-val blue = Color(0xFF4189E1)
-val cyan = Color(0xFF006689)
 val gray = Color(0xFF46464A)
 val black = Color(0xFF1C1C1E)
+
+val red = Color(0xFFFF3A30)
+val orange = Color(0xFFFF9500)
+val yellow = Color(0xFFFFCC00)
+val green = Color(0xFF28CD41)
+val mint = Color(0xFF00C7BD)
+val blue = Color(0xFF4189E1)
+val cyan = Color(0xFF006689)
+val indigo = Color(0xFF5856D6)
+val purple = Color(0xFFAF52DE)
+val brown = Color(0xFFA2855E)
