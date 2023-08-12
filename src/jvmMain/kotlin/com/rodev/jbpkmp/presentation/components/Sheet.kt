aca32546c91f159858551df458d4ea47893258a3
r0astPiGGy <1forzyg1@gmail.com>
2023-08-12 21:32:23 +0300

    Refactoring

--- src/jvmMain/kotlin/com/rodev/jbpkmp/presentation/components/Sheet.kt
+++ src/jvmMain/kotlin/com/rodev/jbpkmp/presentation/components/Sheet.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
